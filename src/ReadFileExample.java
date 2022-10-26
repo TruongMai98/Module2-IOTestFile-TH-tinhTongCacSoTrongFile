2938af9cee22df4662702309a18ecce9ee8903ef
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-10-26 13:50:40 +0700

    [Thực hành] Tính tổng các số trong file text

---
+++ src/ReadFileExample.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class ReadFileExample {
+    public void readFileText(String filePath) {
+        try {
+            File file = new File(filePath);
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line = "";
+            int sum = 0;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                sum += Integer.parseInt(line);
+            }
+            System.out.println("Tổng = " + sum);
+            br.close();
+        } catch (Exception e) {
+            System.out.println("Fie không tồn tại or nội dung có lỗi!");;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Nhập đường dẫn file: ");
+        Scanner scanner = new Scanner(System.in);
+        String path = scanner.nextLine();
+
+        ReadFileExample readFileExample = new ReadFileExample();
+        readFileExample.readFileText(path);
+    }
+
+
+}
